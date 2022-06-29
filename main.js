be1cd326bfe42a656635272e26b0837daceb2cf8
Vlad Czasowski <czasowskij@gmail.com>
2022-06-29 15:15:35 +0200

    testing format

--- main.js
+++ main.js
@@ -28,8 +28,8 @@ async function fun () {
   let supportedVideo;
   if (MediaRecorder.isTypeSupported("video/webm")) supportedVideo = 'video/webm'
   if (MediaRecorder.isTypeSupported("video/mp4")) supportedVideo = 'video/mp4'
-  if (MediaRecorder.isTypeSupported("video/mp4; codecs=avc1.4d002a")) supportedVideo = 'video/mp4; codecs=avc1.4d002a'
-  if (MediaRecorder.isTypeSupported("video/webm;codecs=h264")) supportedVideo = 'video/webm;codecs=h264'
+  // if (MediaRecorder.isTypeSupported("video/mp4; codecs=avc1.4d002a")) supportedVideo = 'video/mp4; codecs=avc1.4d002a'
+  // if (MediaRecorder.isTypeSupported("video/webm;codecs=h264")) supportedVideo = 'video/webm;codecs=h264'
   mimetype.innerText = supportedVideo
   let options = {videoBitsPerSecond: 550000};
   if (supportedVideo) options.mimetype = supportedVideo
